d857f69cb89ef047f0d87b90cf4289a6d403b794
x1mukesh <of.mukesh@gmail.com>
2022-01-29 23:07:46 +0530

    test update

--- src/pages/test.js
+++ src/pages/test.js
@@ -5,28 +5,18 @@ export default class Test extends Component {
     super(props);
     this.state = { date: new Date().toLocaleTimeString() };
   }
-  handleChange() {
-    this.setState({ date: new Date().toLocaleTimeString() });
-  }
   componentDidMount() {
     setInterval(() => {
-      this.handleChange();
+      this.setState({ date: new Date().toLocaleTimeString() });
     }, 1000);
   }
   componentWillUnmount() {
-    console.log(this.state.date);
     clearInterval();
   }
   render() {
     return (
       <div>
-        <div className="card">
-          <div className="card-body">
-            <button onClick={this.handleChange.bind(this)} className="btn">
-              {this.state.date}
-            </button>
-          </div>
-        </div>
+        <h1>{this.state.date}</h1>
       </div>
     );
   }
